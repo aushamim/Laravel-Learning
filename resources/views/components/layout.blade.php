74137d54f6fbd55b190f22f502ad219220cbe233
aushamim <au.shamim0@gmail.com>
2025-05-29 12:13:07 +0600

    formatted code

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +10,7 @@
     {{-- Tailwind --}}
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+
 <body>
     <nav class="bg-gray-200 border-gray-200">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -16,8 +18,9 @@
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap d">Laravel Test</span>
             </a>
-            
-            <ul class="font-medium flex flex-col mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+
+            <ul
+                class="font-medium flex flex-col mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                 <li>
                     <x-nav-link href="/">Home</x-nav-link>
                 </li>
@@ -31,9 +34,10 @@
         </div>
     </nav>
 
-    
+
     <main class="max-w-screen-xl mx-auto p-4">
         {{ $slot }}
     </main>
 </body>
+
 </html>
