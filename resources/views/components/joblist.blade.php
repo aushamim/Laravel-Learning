009141994d5ed91a3f5efc35638d951e2e9906f3
aushamim <au.shamim0@gmail.com>
2025-06-29 11:49:01 +0600

    eager loading

--- resources/views/components/joblist.blade.php
+++ resources/views/components/joblist.blade.php
@@ -1,10 +1,11 @@
 @props(['jobs' => []])
 
-<table>
+{{-- <table>
     <thead>
         <tr>
             <th class="py-2 px-4 border border-gray-300">Position</th>
             <th class="py-2 px-4 border border-gray-300">Yearly Salary</th>
+            <th class="py-2 px-4 border border-gray-300">Employer</th>
         </tr>
     </thead>
     <tbody>
@@ -14,7 +15,17 @@
                     <a href="/jobs/{{ $job['id'] }}" class="hover:text-blue-500 duration-300">{{ $job['title'] }}</a>
                 </td>
                 <td class="py-2 px-4 border border-gray-300">{{ $job['salary'] }}</td>
+                <td class="py-2 px-4 border border-gray-300">{{ $job->employer->name }}</td>
             </tr>
         @endforeach
     </tbody>
-</table>
+</table> --}}
+
+<div class="space-y-4">
+    @foreach ($jobs as $job)
+        <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 hover:border-gray-500 duration-300 rounded-lg">
+            <p class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</p>
+            <p><b>{{ $job['title'] }}</b> pays {{ $job['salary'] ?? "$0" }} per year.</p>
+        </a>
+    @endforeach
+</div>
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,15 @@ use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['jobs' => Job::all()]);
+    $jobs = Job::with('employer')->get();
+
+    return view('home', ['jobs' => $jobs]);
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => Job::all()]);
+    $jobs = Job::with('employer')->get();
+
+    return view('jobs', ['jobs' => $jobs]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
