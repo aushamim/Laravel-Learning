5fbd4f6f77ac43fd4c32c1ccb05644b77069c4e9
aushamim <au.shamim0@gmail.com>
2025-07-08 10:50:01 +0600

    edit and delete job

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,19 +1,23 @@
 <x-layout>
-    <x-slot:title>Job - {{ $job['title'] }}</x-slot:title>
+    <x-slot:title>Job - {{ $job->title }}</x-slot:title>
 
     <h1 class="text-2xl font-semibold">Job Information</h1>
 
     <div class="mt-4">
         <div class="flex gap-2">
             <b>Title: </b>
-            <p>{{ $job['title'] }}</p>
+            <p>{{ $job->title }}</p>
         </div>
         <div class="flex gap-2">
             <b>Salary: </b>
             <p class="flex items-center">
-                {{ $job['salary'] }}
+                {{ $job->salary }}
                 <span class="text-xs font-semibold">/yr</span>
             </p>
         </div>
+    </div>
+
+    <div class="mt-4">
+        <x-button href="/jobs/{{ $job['id'] }}/edit">Edit Job</x-button>
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
     return view('home', ['jobs' => $jobs]);
 });
 
+// Jobs
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->paginate(5);
 
@@ -36,6 +37,34 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    return view('jobs.edit', ['job' => Job::find($id)]);
+});
+Route::patch('/jobs/{id}', function ($id) {
+    // Validation
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/'.$job->id);
+});
+
+// Delete
+Route::delete('/jobs/{id}', function ($id) {
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+});
+
+// Details
 Route::get('/jobs/{id}', function ($id) {
     return view('jobs.show', ['job' => Job::find($id)]);
 });
