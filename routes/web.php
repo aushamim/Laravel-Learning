80a45c48e12ee783856f10a9f4d17333f9e41952
aushamim <au.shamim0@gmail.com>
2025-07-08 11:27:15 +0600

    job controller groups

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [JobController::class, 'index']);
 
 // Jobs
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'delete']);
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'delete');
+// });
+// Same ↑ ↓
+Route::resources('jobs', JobController::class);
 
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
