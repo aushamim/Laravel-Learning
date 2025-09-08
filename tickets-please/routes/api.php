595b91a0258b7b86b824a183112924ac3aaade12
aushamim <au.shamim0@gmail.com>
2025-09-08 11:56:55 +0600

    added Tickets-Please project

---
+++ tickets-please/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Hello, API',
+    ], 200);
+});
+
+Route::get('/login', [AuthController::class, 'login']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
