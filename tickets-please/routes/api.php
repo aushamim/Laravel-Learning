c2dc66e4edc25a8230bf5b0de0f66720efcc2ce0
aushamim <au.shamim0@gmail.com>
2025-09-11 14:53:53 +0600

    create dummy routes

--- tickets-please/routes/api.php
+++ tickets-please/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,14 @@ Route::get('/', function () {
     ], 200);
 });
 
-Route::get('/login', [AuthController::class, 'login']);
+// Authentication
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+// Tickets
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
