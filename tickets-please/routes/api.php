d38454a5f8741025c9a539bc3940b8ec068d6850
aushamim <au.shamim0@gmail.com>
2025-09-15 11:33:28 +0600

    API versioning

--- tickets-please/routes/api.php
+++ tickets-please/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,11 +14,9 @@ Route::get('/', function () {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-// Tickets
-Route::get('/tickets', function () {
-    return Ticket::all();
-});
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// API versioning
+Route::prefix('v1')->group(base_path('routes/api_v1.php'));
