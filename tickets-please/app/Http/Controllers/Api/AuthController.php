84f397128dafbeccf3e161ddf0d56db039a9d40e
aushamim <au.shamim0@gmail.com>
2025-09-16 11:34:40 +0600

    authentication

---
+++ tickets-please/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginUserRequest;
+use App\Models\User;
+use App\Traits\ApiResponses;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('Invalid credentials', 401);
+        }
+
+        $user = User::firstWhere('email', $request->email);
+        $token = $user->createToken('API token for '.$user->email)->plainTextToken;
+
+        return $this->ok('Authenticated', ['token' => $token]);
+    }
+
+    public function register()
+    {
+        return $this->ok('register');
+    }
+}
