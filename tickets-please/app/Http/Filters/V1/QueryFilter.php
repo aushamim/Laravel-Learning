dd78d17b5c49d6c537c40dab95a7cc133c43d84d
aushamim <au.shamim0@gmail.com>
2025-10-05 15:21:34 +0600

    sorting

--- tickets-please/app/Http/Filters/V1/QueryFilter.php
+++ tickets-please/app/Http/Filters/V1/QueryFilter.php
@@ -11,20 +11,11 @@ abstract class QueryFilter
 
     protected $request;
 
+    protected $sortable = [];
+
     public function __construct(Request $request)
     {
         $this->request = $request;
-    }
-
-    protected function filter($arr)
-    {
-        foreach ($arr as $key => $value) {
-            if (method_exists($this, $key)) {
-                $this->$key($value);
-            }
-        }
-
-        return $this->builder;
     }
 
     public function apply(Builder $builder)
@@ -38,5 +29,38 @@ abstract class QueryFilter
         }
 
         return $builder;
+    }
+
+    protected function filter($arr)
+    {
+        foreach ($arr as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
+    protected function sort($value)
+    {
+        $sortAttributes = explode(',', $value);
+
+        foreach ($sortAttributes as $sortAttribute) {
+            $direction = 'asc';
+
+            if (str_starts_with($sortAttribute, '-')) {
+                $direction = 'desc';
+                $sortAttribute = substr($sortAttribute, 1);
+            }
+
+            if (! in_array($sortAttribute, $this->sortable) && ! array_key_exists($sortAttribute, $this->sortable)) {
+                continue;
+            }
+
+            $columnName = $this->sortable[$sortAttribute] ?? $sortAttribute;
+
+            $this->builder->orderBy($columnName, $direction);
+        }
     }
 }
--- tickets-please/routes/api_v1.php
+++ tickets-please/routes/api_v1.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthorsController;
+use App\Http\Controllers\Api\V1\AuthorTicketsController;
 use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('authors', AuthorsController::class)->middleware('auth:sanctum');
+Route::apiResource('authors.tickets', AuthorTicketsController::class)->middleware('auth:sanctum');
 Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
