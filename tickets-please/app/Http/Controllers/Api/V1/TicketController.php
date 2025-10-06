2c9c0901ec90d06cb1f97b25247ebd974a4eea86
aushamim <au.shamim0@gmail.com>
2025-10-06 12:40:31 +0600

    store tickets

--- tickets-please/app/Http/Controllers/Api/V1/TicketController.php
+++ tickets-please/app/Http/Controllers/Api/V1/TicketController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TicketController extends ApiController
 {
@@ -23,7 +25,22 @@ class TicketController extends ApiController
      */
     public function store(StoreTicketRequest $request)
     {
-        //
+        try {
+            $user = User::findOrFail($request->input('data.relationships.author.data.id'));
+        } catch (ModelNotFoundException $e) {
+            return $this->ok('User not found', [
+                'error' => 'The provided user id does not exist.',
+            ]);
+        }
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $request->input('data.relationships.author.data.id'),
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 
     /**
@@ -32,7 +49,7 @@ class TicketController extends ApiController
     public function show(Ticket $ticket)
     {
         if ($this->include('author')) {
-            return new TicketResource($ticket->load('user'));
+            return new TicketResource($ticket->load('author'));
         }
 
         return new TicketResource($ticket);
